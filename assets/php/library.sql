34b40911d569b0453dabe681b9148e20a09fd2ff
Guilherme Braga ferreira <guilhermebragaferreira40@gmail.com>
2024-07-11 23:10:05 -0300

    organizando

---
+++ assets/php/library.sql
@@ -0,0 +1,25 @@
+
+CREATE TABLE categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+CREATE TABLE loans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    book_id INT,
+    loan_date DATE,
+    return_date DATE,
+    returned BOOLEAN DEFAULT FALSE
+);
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL
+);
+CREATE TABLE books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    author VARCHAR(255) NOT NULL,
+    category_id VARCHAR(255) NOT NULL,
+    publication_year INT NOT NULL
+);
